810061b45b41b08ea741f9cb78b80c9179d3e509
Ryan Venturino <ryan.venturino@gmail.com>
2021-02-09 11:32:12 -0500

    updated file structure for better versatility

--- statsAPI/statsFormulas.js
+++ statsAPI/statsFormulas.js
@@ -1,120 +1,202 @@
 //find the mean of an array
 function mean(arr) {
-    return sumArray(arr)/arr.length;
+  return sumArray(arr) / arr.length;
 }
 //median of an array
 function median(arr) {
-    arr.sort();
-    const MID = Math.floor(arr.length / 2);
-    if(arr % 2 === 0){
-        return arr[arr.MID];
-    } else return (arr[MID - 1] + arr[MID]) / 2; 
+  arr.sort();
+  const MID = Math.floor(arr.length / 2);
+  if (arr % 2 === 0) {
+    return arr[arr.MID];
+  } else return (arr[MID - 1] + arr[MID]) / 2;
 }
 //mode of an array
 function mode(arr) {
+  let modes = [],
+    count = [],
+    i,
+    number,
+    maxIndex = 0;
 
-    let modes = [], count = [], i, number, maxIndex = 0;
+  for (i = 0; i < arr.length; i++) {
+    number = arr[i];
+    count[number] = (count[number] || 0) + 1;
+    if (count[number] > maxIndex) {
+      maxIndex = count[number];
+    }
+  }
 
-    for (i = 0; i < arr.length; i++) {
-        number = arr[i];
-        count[number] = (count[number] || 0) + 1;
-        if (count[number] > maxIndex) {
-            maxIndex = count[number];
-        }
+  for (i in count)
+    if (count.hasOwnProperty(i)) {
+      if (count[i] === maxIndex) {
+        modes.push(Number(i));
+      }
     }
 
-    for (i in count)
-        if (count.hasOwnProperty(i)) {
-            if (count[i] === maxIndex) {
-                modes.push(Number(i));
-            }
-        }
-
-    return modes;
+  return modes;
 }
 //range of an array
 function range(numbers) {
-    numbers.sort();
-    return [numbers[0], numbers[numbers.length - 1]];
+  numbers.sort();
+  return [numbers[0], numbers[numbers.length - 1]];
 }
 
 //sum the elements of an array
-function sumArray(arr){
-    let sum = 0;
+function sumArray(arr) {
+  let sum = 0;
 
-    arr.forEach(element => {
-        sum += element;
-    });
-    return sum;
+  arr.forEach((element) => {
+    sum += element;
+  });
+  return sum;
 }
-
-
 
 //provides a list of mid points from a 2 dimensional array, returns a string of midpoints
 // array = [[low limit, high limit, datapoint],[low limit, high limit, datapoint]];
-function listOfMidpoints(arr){
-    let listofpoints = "";
-    let midpoint = 0;
-    let count = 0;
-    for(i=0; i < arr.length; i++){
-        count = arr[i][2];
-        midpoint = arr[i][0] + arr[i][1];
-        for(j=0; j < count; j++){
-            listofpoints += midpoint + ", ";
-        }
+function listOfMidpoints(arr) {
+  let listofpoints = "";
+  let midpoint = 0;
+  let count = 0;
+  for (i = 0; i < arr.length; i++) {
+    count = arr[i][2];
+    midpoint = (arr[i][0] + arr[i][1]) / 2;
+    for (j = 0; j < count; j++) {
+      listofpoints += midpoint + ", ";
     }
-    
-    return listofpoints;
+  }
+
+  return listofpoints;
 }
 
 //variance for population
-function variancePopulation(arr){
-    const MEAN = mean(arr);
-    let topLine = 0;
-    let n = arr.length;
-    let variance = 0;
-    arr.forEach(element => {
-        topLine += Math.pow((element - MEAN), 2);
-    });
-    variance = (topLine/n);
-    return variance;
+function variancePopulation(arr) {
+  const MEAN = mean(arr);
+  let topLine = 0;
+  let n = arr.length;
+  let variance = 0;
+  arr.forEach((element) => {
+    topLine += Math.pow(element - MEAN, 2);
+  });
+  variance = topLine / n;
+  return variance;
 }
 
 //Variance for sample
-function varianceSample(arr){
-    const MEAN = mean(arr);
-    let topLine = 0;
-    let n = arr.length -1;
-    let variance = 0;
-    arr.forEach(element => {
-        topLine += Math.pow((element - MEAN), 2);
-    });
-    variance = (topLine/n);
-    return variance;
+function varianceSample(arr) {
+  const MEAN = mean(arr);
+  let topLine = 0;
+  let n = arr.length - 1;
+  let variance = 0;
+  arr.forEach((element) => {
+    topLine += Math.pow(element - MEAN, 2);
+  });
+  variance = topLine / n;
+  return variance;
 }
 
 //Standard deviation for a population
-function stDevPopulation(arr){
-    return Math.sqrt(variancePopulation(arr));
+function stDevPopulation(arr) {
+  return Math.sqrt(variancePopulation(arr));
 }
 
 //Standard deviation for a Sample
-function stDevSample(arr){
-    return Math.sqrt(varianceSample(arr));
+function stDevSample(arr) {
+  return Math.sqrt(varianceSample(arr));
 }
 
 //coefficient of variation
-function coefficientOfVariation(sdev, mean){
-    return (sdev/mean);
+function coefficientOfVariation(sdev, mean) {
+  return sdev / mean;
 }
 
 //chebychev's theorem
-function chebyshevTheorem(){
-    //k = x - mean / stan dev
-    return (1 - (1/(Math.pow(k, 2))));
+/*
+function chebyshevTheorem() {
+  //k = x - mean / stan dev
+  return 1 - 1 / Math.pow(k, 2);
+}
+*/
+
+function percentile(arr, x) {
+  //top line
+  //find # values < x (a number selected by user)
+  let numLessThanX = 0;
+  let topLineTotal = null;
+  let percentile = null;
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < x) {
+      numLessThanX++;
+    }
+  }
+  // add 0.5
+  topLineTotal = numLessThanX + 0.5;
+  // divide by bottom line
+  percentile = (topLineTotal / arr.length) * 100;
+  // the total num of values
+  //multiply everything by 100
+  return percentile;
 }
 
-console.log("ct: " + chebyshevTheorem(2.5))
+function zscoresample(arr, value) {
+  //top line value - mean of arr
+  //bottom line stdev of arr
+  let topline = 0;
+  let bottomline = 0;
+  let solution = 0;
+
+  topline = value - mean(arr);
+  bottomline = stDevSample(arr);
+  solution = topline / bottomline;
+  return solution;
+}
+
+function valueGivenZ(arr, zscore) {
+  //zscore is x - mean / stdev
+  //so x = mean - z-score * stdev
+  let meany;
+  meany = mean(arr);
+  let stdev;
+  stdev = stDevSample(arr);
+  let solution = meany - zscore * stdev;
+  return solution;
+}
+
+function fiveNumSummary(arr) {
+  let belowq2arr = [];
+  let aboveq2arr = [];
+  let q1;
+  let q2;
+  let q3;
+  let min;
+  let max;
+  let fivenumsum = [];
+  min = arr[0];
+  max = arr[arr.length - 1];
+  q2 = median(arr);
+  arr.forEach((element) => {
+    if (element < q2) {
+      belowq2arr.push(element);
+    }
+  });
+  q1 = median(belowq2arr);
+  arr.forEach((element) => {
+    if (element > q2) {
+      aboveq2arr.push(element);
+    }
+  });
+  q3 = median(aboveq2arr);
+
+  fivenumsum = [
+    ["q1: ", q1],
+    ["q2: ", q2],
+    ["q3: ", q3],
+    ["min: ", min],
+    ["max: ", max],
+  ];
+  return fivenumsum;
+}
+
+//console.log("ct: " + chebyshevTheorem(2.5));
 /*
 TODO
 Chebyshev's theorem
--- statsAPI/tableGenerator.js
+++ statsAPI/tableGenerator.js
@@ -1,70 +1,68 @@
-
 //THIS WILL EVENTUALLY MORPH IN TO THE TABLE GENERATOR MENTIONED IN WHITEBOARD
 //array of values
 //let dataset = [400000,400000,400000,400000,401000,401200,402000,402200,404900,405000,405000,405000,406500,407010,408500,410800,412500,412500,415000,415000,415700,416500,418580,420000,428000,500000,575000,650000,700000,700000,750000,750000,750000,750000,800000,800000,850000,850000,950000,1100000,1125000,1300000,1600000,1750000,1900000,1900000,2000000,2000000,2000000,2100000,2250000,2250000,3000000,3000000,3600000,3650000,3868376,3950000,4312500,4500000,5050000,5250000,5700000,6750000,7700000,12500000,13100000];
 //let dataset = [401000, 404300, 410000, 410000, 410600, 413400, 414120, 417000, 470000, 525000, 550000, 650000, 900000, 1000000, 1000000, 1200000, 1350000, 1500000, 1750000, 1750000, 3000000, 3100000, 3100000, 3517500, 4000000, 4500000, 4875000, 5500000, 7950000, 9375000, 10125000, 11000000, 12000000, 12000000, 13250000, 14500000, 14595953, 15000000, 17775000, 19000000, 20000000, 20625000];
-        //let midpoints = [];
-        let midpoints = [8, 13, 18, 23, 28, 33, 38];
-        //let frequencies = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-        let frequencies = [1, 2, 3, 5, 4, 3, 2];
-        let cumulativefrequencies = [];
-        let relativefrequencies = [];
-        let boundaries = [];
-        let count = 0;
-        let lowBoundaries = [];
-        let highBoundaries = [];
-        var table = document.getElementById("myTable");
-        let data = document.getElementById("otherData");
-        //for the var and sdev
-        let variance = null;
-        let stDev = null;
-        let classfrequencyTimesMidPoint = [];
-        let frequencySquareMidpoint = [];
+//let midpoints = [];
+let midpoints = [8, 13, 18, 23, 28, 33, 38];
+//let frequencies = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let frequencies = [0, 0, 0, 0, 0, 0, 0, 0];
+let cumulativefrequencies = [];
+let relativefrequencies = [];
+let boundaries = [];
+let count = 0;
+let lowBoundaries = [];
+let highBoundaries = [];
+var table = document.getElementById("myTable");
+let data = document.getElementById("otherData");
+//for the var and sdev
+let variance = null;
+let stDev = null;
 
-
-        
-        //loop through the specified rows of data to give them value
-        for(i = 0; i < midpoints.length; i++){
-            //high and low boundaries, hacky way to iterate
-            //lowBoundaries.push(400000 + i*1000000); 
-            //highBoundaries.push(1399999 + i*1000000); 
-            //midpoints. dpes wjat ot meeds to do
-            //midpoints.push(((lowBoundaries[i] + highBoundaries[i])/2)); 
-            // dataset.forEach(element => {
-            //     if(element >= lowBoundaries[i] && element <= highBoundaries[i]){
-            //         frequencies[i] ++;
-            //     }
-            // });
-            classfrequencyTimesMidPoint.push(frequencies[i] * midpoints[i]);
-            frequencySquareMidpoint.push(frequencies[i] * Math.pow(midpoints[i], 2));
-            sumFreq = 0; //counter variable
-            frequencies.forEach(element => {
-                sumFreq += element; 
-            });
-            cumulativefrequencies.push(sumFreq);
-            relativefrequencies.push((frequencies[i]/13).toFixed(3));
-            table.innerHTML += `<tr>
-                                    <td>${accounting.formatMoney(lowBoundaries[i])} | ${accounting.formatMoney(highBoundaries[i])}</td>
-                                    <td>${accounting.formatMoney(midpoints[i])}</td>
+//loop through the specified rows of data to give them value
+for (i = 0; i < 8; i++) {
+  //high and low boundaries, hacky way to iterate
+  lowBoundaries.push(0 + i * 11.5);
+  highBoundaries.push(11.5 + i * 11.5);
+  //midpoints. dpes wjat ot meeds to do
+  midpoints.push((lowBoundaries[i] + highBoundaries[i]) / 2);
+  dataset.forEach((element) => {
+    if (element >= lowBoundaries[i] && element <= highBoundaries[i]) {
+      frequencies[i]++;
+    }
+  });
+  sumFreq = 0; //counter variable
+  frequencies.forEach((element) => {
+    sumFreq += element;
+  });
+  cumulativefrequencies.push(sumFreq);
+  relativefrequencies.push((frequencies[i] / 13).toFixed(3));
+  table.innerHTML += `<tr>
+                                    <td>${accounting.formatMoney(
+                                      lowBoundaries[i]
+                                    )} | ${accounting.formatMoney(
+    highBoundaries[i]
+  )}</td>
+                                    <td>${accounting.formatMoney(
+                                      midpoints[i]
+                                    )}</td>
                                     <td>${frequencies[i]}</td>
                                     <td>${cumulativefrequencies[i]}</td>
                                     <td>${relativefrequencies[i]}</td>
-                                    </tr>`
-                                    
-        } //END OF TABLE GENERATOR ALGORITHM
-        //stdev and var
-        let n = cumulativefrequencies[cumulativefrequencies.length-1];
-        let sumColD;
-        let sumColE;
+                                    </tr>`;
+} //END OF TABLE GENERATOR ALGORITHM
+//stdev and var
+// let n = cumulativefrequencies[cumulativefrequencies.length - 1];
+// let sumColD;
+// let sumColE;
 
-        classfrequencyTimesMidPoint.forEach(element => {
-            sumColD += element;
-        });
-        frequencySquareMidpoint.forEach(element => {
-            sumColE += element;
-        });
-        variance = ((n * (sumColE)) - (Math.pow(sumColD, 2)))/ (n * (n -1));
-        stDev = Math.sqrt(variance);
-        console.log("variance: " + variance);
-        console.log("stdev: " + stDev);
-        console.log(5.5+5)
+// classfrequencyTimesMidPoint.forEach((element) => {
+//   sumColD += element;
+// });
+// frequencySquareMidpoint.forEach((element) => {
+//   sumColE += element;
+// });
+// variance = (n * sumColE - Math.pow(sumColD, 2)) / (n * (n - 1));
+// stDev = Math.sqrt(variance);
+// console.log("variance: " + variance);
+// console.log("stdev: " + stDev);
+// console.log(5.5 + 5);
